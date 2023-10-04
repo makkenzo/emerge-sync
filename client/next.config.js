b4076e53d797ecd215a127ee8e3caeafb8174a4d
makkenzo <nekgo2009@gmail.com>
2023-10-04 16:50:10 +0600

    init2

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/auth/:path',
+                destination: '/:path',
+            },
+        ];
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/auth',
+                permanent: true,
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ client/pages/auth.tsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+    return (
+        <div>
+            <h1 className="text-red-800">Auth page</h1>
+        </div>
+    );
+};
+
+export default Auth;
