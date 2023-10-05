296b73c557a65ada31d89b0c97783ee7573c7535
makkenzo <nekgo2009@gmail.com>
2023-10-05 17:29:23 +0600

    syntax change

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -6,18 +6,16 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 const dashboard = () => {
-    const isAuthenticated = useSelector((state: RootState) => state.auth.token);
+    const token = useSelector((state: RootState) => state.auth.token);
+    const userId = useSelector((state: RootState) => state.auth.userId);
 
     const router = useRouter();
 
     useEffect(() => {
-        if (!isAuthenticated) {
+        if (!token) {
             router.push('/auth');
         }
     }, []);
-
-    // const userId = useSelector((state: RootState) => state.auth.userId);
-    // console.log(userId);
 
     return (
         <div>
