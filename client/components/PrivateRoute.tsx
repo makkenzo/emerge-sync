921543428229027a2784f78af8e77f14add5bcff
makkenzo <nekgo2009@gmail.com>
2023-10-05 15:34:30 +0600

    removed unused feature

--- client/components/PrivateRoute.tsx
+++ client/components/PrivateRoute.tsx
@@ -1,19 +1,19 @@
-import { ReactNode, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { useRouter } from 'next/router';
-import { RootState } from '../redux/store';
+// import { ReactNode, useEffect } from 'react';
+// import { useSelector } from 'react-redux';
+// import { useRouter } from 'next/router';
+// import { RootState } from '../redux/store';
 
-const PrivateRoute = ({ children }: { children: ReactNode }) => {
-    const router = useRouter();
-    const token = useSelector((state: RootState) => state.auth.token);
+// const PrivateRoute = ({ children }: { children: ReactNode }) => {
+//     const router = useRouter();
+//     const token = useSelector((state: RootState) => state.auth.token);
 
-    useEffect(() => {
-        if (!token) {
-            router.push('/auth');
-        }
-    }, [token, router]);
+//     useEffect(() => {
+//         if (!token) {
+//             router.push('/auth');
+//         }
+//     }, [token, router]);
 
-    return <>{children}</>;
-};
+//     return <>{children}</>;
+// };
 
-export default PrivateRoute;
+// export default PrivateRoute;
