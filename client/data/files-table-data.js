777fcc7317e452c81095295776b46b20b977995b
makkenzo <nekgo2009@gmail.com>
2023-10-06 16:36:15 +0600

    sample data

---
+++ client/data/files-table-data.js
@@ -0,0 +1,40 @@
+export const filesTableData = [
+    {
+        file: 'Quarterly_Report_Q1_2023.xlsx',
+        assignedTo: 'John Michael',
+        imgAssignedTo: '/users/6f7.jpg',
+        date: '23/04/2023',
+    },
+    {
+        file: 'Budget_Plan_2023.xlsx',
+        assignedTo: 'Alexa Liras',
+        imgAssignedTo: '/users/1616247568681.jpeg',
+        date: '11/01/2023',
+    },
+    {
+        file: 'Sales_Forecast_Q2_2023.xlsx',
+        assignedTo: 'Laurent Perrier',
+        imgAssignedTo: '/users/1665806071444.jpeg',
+        date: '19/09/2023',
+    },
+    {
+        file: 'Project_Schedule.xlsx',
+        assignedTo: 'Michael Levi',
+        imgAssignedTo: '/users/YH5TFCE1QY-preview.png',
+        date: '24/12/2023',
+    },
+    {
+        file: 'Financial_Statement_2023.xlsx',
+        assignedTo: 'Bruce Mars',
+        imgAssignedTo: '/users/sample-profile-pic.png',
+        date: '04/10/2023',
+    },
+    {
+        file: 'Product_Inventory.xlsx',
+        assignedTo: 'Alexander',
+        imgAssignedTo: '/users/sample-profile-picture.png',
+        date: '14/09/2023',
+    },
+];
+
+export default filesTableData;
--- client/pages/dashboard/files.tsx
+++ client/pages/dashboard/files.tsx
@@ -1,13 +1,61 @@
 import { Sidenav } from '@/components';
-import React from 'react';
+import { Card, CardBody, CardHeader, Typography } from '@material-tailwind/react';
+import { filesTableData } from '@/data/files-table-data';
+import Image from 'next/image';
 
 const files = () => {
     return (
         <div className="min-h-screen bg-slate-200 flex">
             <Sidenav />
-            {/* ...files table here */}
+
             <div className="container mx-auto py-8">
-                <h1>FILES</h1>
+                <div className="mt-4 mb-8 flex flex-col gap-12">
+                    <Card>
+                        <CardHeader variant="filled" color="blue-gray" className="mb-8 p-6">
+                            Файлы
+                        </CardHeader>
+                        <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+                            <table className="w-full min-w-[640px] table-auto">
+                                <thead>
+                                    <tr>
+                                        {['имя', 'прикреплен к', 'действие'].map((el) => (
+                                            <th key={el} className="border-b border-blue-gray-50 py-3 px-5 text-left">
+                                                <Typography
+                                                    variant="small"
+                                                    className="text-[11px] font-bold uppercase text-blue-gray-400"
+                                                >
+                                                    {el}
+                                                </Typography>
+                                            </th>
+                                        ))}
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {filesTableData.map((file, index) => (
+                                        <tr key={index} className="border-b border-blue-gray-100">
+                                            <td className="py-3 px-5 text-left">{file.file}</td>
+                                            <td className="py-3 px-5 text-left flex items-center">
+                                                <div className="w-8 h-8 rounded-full overflow-hidden mr-2">
+                                                    <Image
+                                                        src={file.imgAssignedTo}
+                                                        alt="pfp"
+                                                        width={80}
+                                                        height={80}
+                                                        className="object-contain"
+                                                    />
+                                                </div>
+                                                {file.assignedTo}
+                                            </td>
+                                            <td className="py-3 px-5 text-left">
+                                                {/* Здесь вы можете добавить действие, если необходимо */}
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
         </div>
     );
