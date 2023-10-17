215594137bc0f89598b1f1382797acee98e1dc6f
makkenzo <nekgo2009@gmail.com>
2023-10-17 17:48:31 +0600

    sharp server

---
+++ EmergeSyncAPI/EmergeSyncAPI/Models/EmergeSyncDBSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace EmergeSyncAPI.Models;
+
+public class EmergeSyncDBSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string UsersCollectionName { get; set; } = null!;
+}
---
+++ EmergeSyncAPI/EmergeSyncAPI/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace EmergeSyncAPI.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string UserName { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
