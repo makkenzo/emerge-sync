09b53fa9e2c6c46989ebd7dce00dbab4c7f83c56
makkenzo <nekgo2009@gmail.com>
2023-10-26 15:45:45 +0600

    editable cell

--- client/pages/dashboard/file/[file_id].tsx
+++ client/pages/dashboard/file/[file_id].tsx
@@ -13,14 +13,31 @@ const FilePage = () => {
     const fileId = router.query.file_id;
 
     const [document, setDocument] = useState<{ [key: string]: any }[]>([]);
-    const [editCell, setEditCell] = useState({ rowIndex: -1, colIndex: -1 });
+
+    const [isEditing, setIsEditing] = useState<{ row: number; col: number } | null>(null);
+    const [editedData, setEditedData] = useState<string | number>('');
+
+    const handleDoubleClick = (rowIndex: number, colIndex: number) => {
+        setIsEditing({ row: rowIndex, col: colIndex });
+        setEditedData(document[rowIndex][Object.keys(document[0])[colIndex]]);
+    };
+
+    const handleBlur = () => {
+        if (isEditing) {
+            const { row, col } = isEditing;
+            const updatedDocument = [...document];
+            updatedDocument[row][Object.keys(document[0])[col]] = editedData;
+            setDocument(updatedDocument);
+            setIsEditing(null);
+        }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get(`http://localhost:5000/api/v1/documents/get-document/${fileId}`);
                 setDocument(response.data);
-                console.log(response.data);
+                // console.log(response.data);
             } catch (error) {
                 console.error(error);
             }
@@ -34,9 +51,8 @@ const FilePage = () => {
     const handleSave = async () => {
         try {
             console.log(document);
-
-            await axios.put(`http://localhost:5000/api/v1/documents/update-document/${fileId}`, document);
-            window.location.reload();
+            // await axios.put(`http://localhost:5000/api/v1/documents/update-document/${fileId}`, document);
+            // window.location.reload();
         } catch (error) {
             console.error('Failed to save document:', error);
         }
@@ -58,10 +74,12 @@ const FilePage = () => {
                             className="mb-8 p-6 flex justify-between items-center"
                         >
                             Документ
-                            <Button onClick={handleSave}>Сохранить</Button>
+                            <div className="">
+                                <Button onClick={handleSave}>Сохранить</Button>
+                            </div>
                         </CardHeader>
                         <CardBody className="px-0 pt-0 pb-2 gap-4">
-                            <div className="w-full h-[810px] overflow-x-auto">
+                            <div className="w-full h-[795px] overflow-x-auto">
                                 {document && document.length > 0 ? (
                                     <table className="w-max table-auto">
                                         <tbody>
@@ -75,8 +93,29 @@ const FilePage = () => {
                                             {document.map((item, rowIndex) => (
                                                 <tr key={rowIndex} className="border border-blue-gray-100">
                                                     {Object.values(item).map((value, colIndex) => (
-                                                        <td key={colIndex} className="py-3 px-5 text-left border">
-                                                            {value}
+                                                        <td
+                                                            key={colIndex}
+                                                            className={`py-3 px-5 text-left border ${
+                                                                isEditing &&
+                                                                isEditing.row === rowIndex &&
+                                                                isEditing.col === colIndex
+                                                                    ? 'bg-yellow-100'
+                                                                    : ''
+                                                            }`}
+                                                            onDoubleClick={() => handleDoubleClick(rowIndex, colIndex)}
+                                                        >
+                                                            {isEditing &&
+                                                            isEditing.row === rowIndex &&
+                                                            isEditing.col === colIndex ? (
+                                                                <input
+                                                                    type="text"
+                                                                    value={editedData}
+                                                                    onChange={(e) => setEditedData(e.target.value)}
+                                                                    onBlur={handleBlur}
+                                                                />
+                                                            ) : (
+                                                                value
+                                                            )}
                                                         </td>
                                                     ))}
                                                 </tr>
